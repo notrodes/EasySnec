b7fdff03cfbc9c96a1bde077c7551d671905d8c9
Quinn Bowers <q.bowers@mailbox.org>
2026-01-22 01:07:23 -0500

    minor build script fixes

--- scripts/build_pyapp.ps1
+++ scripts/build_pyapp.ps1
@@ -3,15 +3,17 @@ $version=$(uvx hatch version)
 
 uv build
 
-pwsh -Command {
-    $env:PYAPP_DISTRIBUTION_EMBED="true";
-    $env:PYAPP_PROJECT_PATH="$(pwd)/dist/easysnec-$version.tar.gz";
-    $env:PYAPP_UV_ENABLED="true";
-    $env:PYAPP_PYTHON_VERSION="3.11";
-    uvx hatch build -t binary
-    # $env:MYVAR="myvalue";
-    # .\path\to.exe
-}
+# # pwsh -Command {
+$env:PYAPP_DISTRIBUTION_EMBED="true";
+$env:PYAPP_PROJECT_PATH="$(pwd)/dist/easysnec-$version.tar.gz";
+$env:PYAPP_UV_ENABLED="true";
+$env:PYAPP_PYTHON_VERSION="3.11";
+$env:PYAPP_IS_GUI="true";
+uvx hatch build -t binary
+# # $env:MYVAR="myvalue";
+# # .\path\to.exe
+# # }
 
 # you need to install innosetup and find its installation location
+# $env:USERPROFILE\AppData\Local\Programs\"Inno Setup 6"\ISCC.exe /DVersionNo=$version /O$(pwd)/dist/installers build/windows_installer.iss
 C:\Users\qbowers\AppData\Local\Programs\"Inno Setup 6"\ISCC.exe /DVersionNo=$version /O$(pwd)/dist/installers build/windows_installer.iss
